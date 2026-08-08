f5ed829379fefe3b6dff64a1153f2da4e27131d8
komalshukla2005 <komalshukla7518046264@gmail.com>
2026-08-08 22:54:58 +0530

    done

--- backend/server.js
+++ backend/server.js
@@ -6,15 +6,23 @@ import authRoutes from './src/auth/authRoutes.js';
 import contactRoutes from './src/contact/contactRoutes.js';
 
 dotenv.config();
+
 connectDB();
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
+
+// GET API
+app.get('/', (req, res) => {
+    res.send("PDFTools  is running successfully")
+});
 
 app.use('/api/auth', authRoutes);
 app.use('/api/tools', authRoutes);
 app.use('/api/contact', contactRoutes);
 
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
